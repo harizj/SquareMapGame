f713a27fa0705e43bb6ab7ca83dd0eed32469381
hariz <harizjohnson@gmail.com>
2026-05-20 13:29:31 -0400

    Trade routes visible to own faction

--- src/game/line_of_sight.py
+++ src/game/line_of_sight.py
@@ -36,11 +36,17 @@ class LineOfSight:
                     )
                     visible.update(reachable.keys())
 
+        seen_routes = set()
         for (r, c), city in game_map.cities.items():
             if city.faction is faction:
                 reachable = game_map.get_reachable_from(
                     r, c, CITY_VISION_DISTANCE, mode='any', include_start=True
                 )
                 visible.update(reachable.keys())
+                for route in city.trade_routes:
+                    if id(route) not in seen_routes:
+                        seen_routes.add(id(route))
+                        if route.established:
+                            visible.update(route.path)
 
         return visible
