38f66d220775f887ac08d4bf44a8f0a3bc4866af
hariz <harizjohnson@gmail.com>
2026-06-05 14:52:45 -0400

    Scatter in cardinal directions

--- map_generation/tutorial_temperate.py
+++ map_generation/tutorial_temperate.py
@@ -1,5 +1,7 @@
 from map_generation.builder import MapBuilder
 
+_CARDINAL = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
 
 def generate(game_map):
     b = MapBuilder(game_map)
@@ -7,14 +9,20 @@ def generate(game_map):
 
     b.zone(range(0, 11), biome='temperate')
 
+    # Place a mountain at the map center
+    center = b._map.tiles[5][5]
+    center.terrain_features = list(center.terrain_features) + ['mountain']
+    center.update_terrain_properties()
+    center.build_deposits()
+
     # Light scatter of forest, hills, and mountains
-    b.scatter('mountain', biome='temperate', density=0.03)
-    b.scatter('mountain', biome='temperate', density=0.2, requires_neighbor='mountain')
-    b.scatter('hills', biome='temperate', density=0.4, requires_neighbor='mountain')
-    b.scatter('hills', biome='temperate', density=0.1)
-    b.scatter('forest', biome='temperate', density=0.1)
-    b.scatter('forest', biome='temperate', density=0.25, requires_neighbor='forest')
-    #b.scatter('hills',    biome='temperate', density=0.15)
+    b.scatter('mountain', biome='temperate', density=0.5, requires_neighbor='mountain', neighbor_dirs=_CARDINAL)
+    b.scatter('hills', biome='temperate', density=0.5, requires_neighbor='mountain', neighbor_dirs=_CARDINAL)
+    b.scatter('hills', biome='temperate', density=0.05)
+    b.scatter('hills', biome='temperate', density=0.25, requires_neighbor='hills', neighbor_dirs=_CARDINAL)
+
+    b.scatter('forest', biome='temperate', density=0.15)
+    b.scatter('forest', biome='temperate', density=0.5, requires_neighbor='forest', neighbor_dirs=_CARDINAL)
 
     # Iron on some hill tiles
     b.scatter('iron', density=0.40, requires='hills')
